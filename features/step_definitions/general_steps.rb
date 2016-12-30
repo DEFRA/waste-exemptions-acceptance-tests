e9c5fe62394b21cfa286600934692fb87465dbee
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2016-12-30 10:30:37 +0000

    Merge BO and FO general_steps.rb into one file
    This change merges the BO and FO general steps into one file at the root of the steps folder. Currently the majority of the steps are general to both so this gives a starte for ten before then branching off into specific front and back office versions.

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -1,3 +1,10 @@
+Given(/^I am an external user$/) do
+
+  @app = App.new
+  @app.front_office_home_page.load
+
+end
+
 Given(/^I am an internal user$/) do
 
   @app = App.new
@@ -8,6 +15,50 @@ Given(/^I am an internal user$/) do
     user_email: "tim.stone.ea@gmail.com",
     user_password: "Abcde12345"
   )
+
+end
+
+Given(/^I select a new exemption$/) do
+
+  @app.registration_type_page.submit_new
+
+end
+
+When(/^I register an exemption$/) do
+
+  @app.organisation_name_page.submit(
+    org_name: "Mr Test"
+  )
+
+  @app.address_page.submit_address(
+    postcode: "BS1 5AH",
+    result: "ENVIRONMENT AGENCY, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH"
+  )
+
+  @app.correspondence_contact_page.submit(
+    tel_number: "01234567899"
+  )
+
+  @app.farm_questions_page.submit
+
+  @app.site_location_page.submit_grid_ref(
+    grid_ref: "ST5820572708",
+    site_details: "by a tree near a river there's a hole in the ground"
+  )
+
+  @app.choose_exemptions_page.submit(
+    tab: :using_waste,
+    exemption: "U1"
+  )
+
+  @app.check_details_page.submit
+  @app.declaration_page.submit
+
+end
+
+Then(/^I will be informed the registration is complete$/) do
+
+  expect(page).to have_content "Registration complete"
 
 end
 
@@ -60,8 +111,3 @@ When(/^I complete a registration$/) do
 
 end
 # rubocop:enable Metrics/BlockLength
-
-Then(/^I will be informed the registration is complete$/) do
-  expect(page).to have_content "Registration complete"
-
-end
