553a1abf38d4be1a1832545666be7532e096dfa4
Tim <tim.stone@yahoo.co.uk>
2017-01-05 15:19:39 +0000

    Feature/ngr lookup (#3)
    Back office feature to test site address lookup of National Grid reference and area

---
+++ features/backoffice_site_address.feature
@@ -0,0 +1,21 @@
+@backoffice
+Feature: National grid reference and site area details are found from site postcode
+  As a back office user
+  I want to know the National grid refence (NGR) and area
+  So that I can check the activity location for exemption restrictions
+
+  Background:
+    Given I am an internal user
+      And I have a valid username and password
+
+  @happy_path
+  Scenario Outline: Registration by a NCCC user using a postcode for site address - NGR and area is added to registration
+     When I complete a registration using postcode <postcode> for the site address
+     Then I will see the site's National Grid Reference has been automatically found to be <ngr>
+     And I will see the EA admin area is set to <area>
+
+     Examples:
+     | postcode | ngr           | area                                         |
+     | BS1 5AH  | ST5813072687  | wessex                                       |
+     | CV35 9ES | SP2518455455  | Staffordshire Warwickshire and West Midlands |
+     | SA17 5AF | SN4104108640  | Outside England                              |
--- features/step_definitions/back_office/general_steps.rb
+++ features/step_definitions/back_office/general_steps.rb
@@ -56,4 +56,63 @@ When(/^I complete a registration$/) do
   @exemption_number = @app.confirmation_page.ref_no.text
 
 end
+
+When(/^I complete a registration using postcode (.*) for the site address$/) do |postcode|
+  @app.search_page.nav_bar.registrations_menu.click
+  @app.search_page.nav_bar.new_option.click
+
+  @app.contact_details_page.submit(
+    full_name: "Mr Test",
+    tel_number: "01234567899",
+    email: "tim.stone.ea@gmail.com",
+    confirm_email: "tim.stone.ea@gmail.com"
+  )
+
+  @app.user_type_page.submit(
+    org_type: "WasteExemptionsShared::OrganisationType::Individual"
+  )
+
+  @app.organisation_name_page.submit(
+    org_name: "Mr Test"
+  )
+
+  @app.address_page.submit_address(
+    postcode: "BS1 5AH",
+    result: "ENVIRONMENT AGENCY, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH"
+  )
+
+  @app.correspondence_contact_page.submit(
+    tel_number: "01234567899"
+  )
+
+  @app.farm_questions_page.submit
+
+  @app.site_location_page.submit_address(
+    postcode: postcode
+  )
+
+  @app.choose_exemptions_page.submit(
+    tab: :storing_waste,
+    exemption: "S1"
+  )
+
+  @app.check_details_page.submit
+  @app.declaration_page.submit
+
+  @exemption_number = @app.confirmation_page.ref_no.text
+
+end
+
+Then(/^I will see the site's National Grid Reference has been automatically found to be (.*)$/) do |ngr|
+  @app.search_page.nav_bar.home_link.click
+  @app.search_page.search_field.set @exemption_number
+  @app.search_page.search_button.click
+  @app.search_page.first_search_result.click
+  @app.registration_details_page.site_section.click
+  expect(@app.registration_details_page.ngr.text).to include(ngr)
+end
+
+And(/^I will see the EA admin area is set to (.*)$/) do |area|
+  expect(@app.registration_details_page.area.text).to include(area)
+end
 # rubocop:enable Metrics/BlockLength
