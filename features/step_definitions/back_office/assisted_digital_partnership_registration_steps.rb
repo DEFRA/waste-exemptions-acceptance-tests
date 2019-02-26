8089bb6092b8ed2eeaca630fe78be3a5b3ededba
Tim <tim.stone@yahoo.co.uk>
2019-02-26 08:18:32 +0000

    adding more business type ad registrations (#119)

--- features/step_definitions/back_office/assisted_digital_partnership_registration_steps.rb
+++ features/step_definitions/back_office/assisted_digital_partnership_registration_steps.rb
@@ -1,4 +1,4 @@
-When(/I complete a limited company registration$/) do
+When(/^I complete a partnerships registration$/) do
   @email_address = generate_email
   @app.registration_dashboard_page.create_new_registration.click
   @app.registration_type_page.submit_new
@@ -10,25 +10,23 @@ When(/I complete a limited company registration$/) do
     email: @email_address,
     confirm_email: @email_address
   )
-  @app.business_type_page.submit(business_type: :limited)
-  @app.registration_number_page.submit(
-    registration_number: "00445790"
-  )
+  @app.business_type_page.submit(business_type: :partnership)
+  people = @app.main_people_page.main
+  @app.main_people_page.add_main_person(person: people[0])
+  @app.main_people_page.submit(person: people[1])
+
   @app.operator_name_page.submit(
-    org_name: "Testco"
+    org_name: "Test partnership"
   )
-  @app.operator_address_page.submit_manual_address(
+  @app.operator_address_page.submit(
     postcode: "BS1 5AH",
-    house_no: "1",
-    address_line_one: "test lane",
-    address_line_two: "teston",
-    city: "testville"
+    result: "NATURAL ENGLAND, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH"
   )
   @app.contact_name_page.submit(
     first_name: "Bob",
     last_name: "Cratchett"
   )
-  @app.contact_position_page.submit(position: "Manager")
+  @app.contact_position_page.submit(position: "Partner")
   @app.contact_telephone_page.submit(tel_no: "0117 9000000")
   @app.contact_email_page.submit(
     email: @email_address,
