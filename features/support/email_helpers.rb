16cd2a94ab461ae9a5e0eb7548a060f2474bd362
Andrew Hick <andrewhick.ea@gmail.com>
2020-11-13 10:01:08 +0000

    Remove unnecessary puts

--- features/support/email_helpers.rb
+++ features/support/email_helpers.rb
@@ -24,12 +24,10 @@ def email_exists?(app, registration, expected_text)
   # We don't know whether the applicant or contact email will be sent first, so try both.
   # Try the applicant email:
   expected_text_for_applicant = expected_text + [registration[:applicant][:email]]
-  puts "Expected text for applicant: " + expected_text_for_applicant.to_s
   return true if @world.journey.last_notify_msg_page.email_has_text?(expected_text_for_applicant)
 
   # If that doesn't work, try the contact email:
   expected_text_for_contact = expected_text + [registration[:contact][:email]]
-  puts "Expected text for contact  : " + expected_text_for_contact.to_s
   return true if @world.journey.last_notify_msg_page.email_has_text?(expected_text_for_contact)
 
   puts "Email not found"
