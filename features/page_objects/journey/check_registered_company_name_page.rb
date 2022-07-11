a7b87a0c7d75721564c1df26522fdebe9868eb3f
Tim <tim.stone@yahoo.co.uk>
2022-07-11 16:46:44 +0100

    Removing linting exception

--- features/page_objects/journey/check_registered_company_name_page.rb
+++ features/page_objects/journey/check_registered_company_name_page.rb
@@ -3,11 +3,11 @@
 class CheckRegisteredCompanyNamePage < BasePage
 
   # Is this your registered name and address?
-  # rubocop:disable Layout/LineLength
-  element(:confirm_company_details, "#check-registered-name-and-address-form-temp-use-registered-company-details-true-field", visible: false)
-  element(:reject_company_details, "#check-registered-name-and-address-form-temp-use-registered-company-details-field", visible: false)
+
+  element(:confirm_company_details, "input[id$='company-details-true-field']", visible: false)
+  element(:reject_company_details, "input[id$='company-details-field']", visible: false)
   element(:companies_house_number, ".govuk-heading-m:nth-child(1)")
-  # rubocop:enable Layout/LineLength
+
   def submit(args = {})
     case args[:choice]
     when :reject
