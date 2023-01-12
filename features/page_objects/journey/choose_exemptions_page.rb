d21731e896950f356dd102476b88d9d8dbdf8a1b
Tim <tim.stone@yahoo.co.uk>
2023-01-12 16:49:43 +0000

    Deselecting exemptions during renewal

--- features/page_objects/journey/choose_exemptions_page.rb
+++ features/page_objects/journey/choose_exemptions_page.rb
@@ -2,16 +2,40 @@
 
 class ChooseExemptionsPage < BasePage
 
-  elements(:exemptions, "input[name='exemptions_form[exemption_ids][]']", visible: false)
+  elements(:exemptions, "input[name$='exemptions_form[exemption_ids][]']", visible: false)
 
   def submit(args = {})
     if args.key?(:exemptions)
       args[:exemptions].each do |ex|
-        search_val = "exemptions_form_checkbox-#{ex}"
+
+        search_val = if args.key?(:renewal)
+                       "renew_exemptions_form_checkbox-#{ex}"
+                     else
+                       "exemptions_form_checkbox-#{ex}"
+                     end
+                     puts search_val
         exemptions.find { |chk| chk["id"] == search_val }.click
       end
     end
 
+    submit_button.click
+  end
+
+  # def renew(args = {})
+  #   if args.key?(:exemptions)
+  #     args[:exemptions].each do |ex|
+  #       search_val = "renew_exemptions_form_checkbox-#{ex}"
+  #       exemptions.find { |chk| chk["id"] == search_val }.click
+  #     end
+  #   end
+
+  #   submit_button.click
+  # end
+
+  def uncheck_all_exemptions_and_submit
+    exemptions.each do |ex|
+      ex.click if ex.checked?
+    end
     submit_button.click
   end
 
