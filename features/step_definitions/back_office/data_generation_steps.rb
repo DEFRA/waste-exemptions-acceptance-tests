abaf4c5acf0a2002be4694d4e07df603dc99556b
Tim <tim.stone@yahoo.co.uk>
2023-03-13 15:59:50 +0000

    Final scenario

--- features/step_definitions/back_office/data_generation_steps.rb
+++ features/step_definitions/back_office/data_generation_steps.rb
@@ -1,19 +1,16 @@
 # frozen_string_literal: true
 
-Given("I have a registration outside of the renewal window") do
+Given("I have a registration {string} of the renewal window") do |window|
   login_user(@world.developer_user)
-  #   Expiry window in test environment is set to three years so expiry date needs to be longer
-  @four_years = DateTime.now.next_year(4).strftime("%Y-%m-%d")
-  create_registration(@four_years)
+  @years = if window == "outside"
+             #   Expiry window in test environment is set to three years so expiry date needs to be longer
+             DateTime.now.next_year(4).strftime("%Y-%m-%d")
+           else
+             DateTime.now.next_year(2).strftime("%Y-%m-%d")
+           end
+  create_registration(@years)
   @registration = @world.bo.dashboard_page.created_registration.text[/(WEX\d+)/, 1]
+  @business_type = :limited_company
   puts "#{@registration} generated"
-end
-
-Given("I have a registration inside of the renewal window") do
-  login_user(@world.developer_user)
-  #   Expiry window in test environment is set to three years so expiry date needs to be longer
-  @two_years = DateTime.now.next_year(2).strftime("%Y-%m-%d")
-  create_registration(@two_years)
-  @registration = @world.bo.dashboard_page.created_registration.text[/(WEX\d+)/, 1]
-  puts "#{@registration} generated"
+  @world.last_reg_no = @registration
 end
