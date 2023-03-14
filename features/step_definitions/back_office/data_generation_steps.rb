391c676bf3ee767df9ead4c749149a5a84b4a091
Tim <tim.stone@yahoo.co.uk>
2023-03-14 08:47:46 +0000

    Deregistration data generation update (#310)

--- features/step_definitions/back_office/data_generation_steps.rb
+++ features/step_definitions/back_office/data_generation_steps.rb
@@ -2,6 +2,8 @@
 
 Given("I have a registration {string} of the renewal window") do |window|
   login_user(@world.developer_user)
+  # clears out persisted registration
+  @world.last_reg = nil unless @world.last_reg.nil?
   @years = if window == "outside"
              #   Expiry window in test environment is set to three years so expiry date needs to be longer
              DateTime.now.next_year(4).strftime("%Y-%m-%d")
