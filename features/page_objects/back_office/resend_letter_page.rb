1fa57a794e26b82b43952555b8b4282148bef75f
Tim <tim.stone@yahoo.co.uk>
2024-01-04 14:32:23 +0000

    Updated action selectors

--- features/page_objects/back_office/resend_letter_page.rb
+++ features/page_objects/back_office/resend_letter_page.rb
@@ -5,11 +5,11 @@ require_relative "sections/admin_menu_section"
 class ResendLetterPage < BasePage
 
   def resend_renewal_letter(registration_number)
-    find(:css, "[href='/resend-renewal-letter/#{registration_number}']")
+    find(:css, "[href^='/resend-renewal-letter/#{registration_number}']")
   end
 
   def resend_confirmation_letter(registration_number)
-    find(:css, "[href='/resend-confirmation-letter/#{registration_number}']")
+    find(:css, "[href^='/resend-confirmation-letter/#{registration_number}']")
   end
 
 end
