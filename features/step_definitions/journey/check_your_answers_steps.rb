7e4b7fcf3f0995a9c4cd3827dda8a0e03481ac0b
Tim <tim.stone@yahoo.co.uk>
2024-05-21 09:54:13 +0100

    linting fix

--- features/step_definitions/journey/check_your_answers_steps.rb
+++ features/step_definitions/journey/check_your_answers_steps.rb
@@ -4,7 +4,7 @@ When("I change the contact name previously entered") do
   @world.journey.check_details_page.change_contact_name.click
   @new_contact_name = generate_person("new_contact@example.com")
   @world.journey.contact_name_page.submit(first_name: @new_contact_name[:first_name],
-                                                last_name: @new_contact_name[:last_name])
+                                          last_name: @new_contact_name[:last_name])
 end
 
 Then("I can see the contact name has been updated") do
