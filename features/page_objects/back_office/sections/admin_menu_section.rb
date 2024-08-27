cf6b498d8b0465ec091e82b5882715aeabf06ffd
Tim <tim.stone@yahoo.co.uk>
2024-08-27 13:12:00 +0100

    Design system update (#374)

--- features/page_objects/back_office/sections/admin_menu_section.rb
+++ features/page_objects/back_office/sections/admin_menu_section.rb
@@ -6,7 +6,7 @@ class AdminMenuSection < SitePrism::Section
 
   SELECTOR = ".govuk-header"
 
-  element(:home_page, ".govuk-header__link--service-name")
+  element(:home_page, "#proposition-name")
   element(:user_management, "a[href='/users']")
   element(:dashboard_link, "li:nth-child(1) a")
   element(:data_exports, "a[href*='/data-exports']")
