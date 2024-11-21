3338ac58ffd3f8636e78e5c8ebf564fa3a7db4ef
Tim Stone <tim.stone@yahoo.co.uk>
2024-11-21 11:43:06 +0000

    Remove exemption references in charging summary (#381)

--- features/step_definitions/journey/charge_summary_steps.rb
+++ features/step_definitions/journey/charge_summary_steps.rb
@@ -19,20 +19,16 @@ end
 Then("I can see the total charge is £{float}") do |charge|
   expect(trim_comma(@world.journey.exemptions_summary_page.total_charge.text)).to include(charge.to_s)
 end
-# rubocop:disable Layout/LineLength
-Then("I can see that the {string} compliance charge of band {int} exemption {string} is £{float}") do |charge_type, band, exemption, charge|
+Then("I can see that the {string} compliance charge of band {int} exemption is £{float}") do |charge_type, band, charge|
   case charge_type
   when "Full"
     expect(@world.journey.exemptions_summary_page.charge_types.first.text).to have_text(charge_type)
-    expect(@world.journey.exemptions_summary_page.exemptions.first.text).to have_text(exemption)
     expect(trim_comma(@world.journey.exemptions_summary_page.charge_amounts.first.text)).to include(charge.to_s)
     expect(@world.journey.exemptions_summary_page.bands.first.text).to have_text(band)
   when "Discounted"
     expect(@world.journey.exemptions_summary_page.charge_types.last.text).to have_text(charge_type)
-    expect(@world.journey.exemptions_summary_page.exemptions.last.text).to have_text(exemption)
     expect(@world.journey.exemptions_summary_page.charge_amounts.last.text).to include(charge.to_s)
     expect(@world.journey.exemptions_summary_page.bands.last.text).to have_text(band)
   end
 
 end
-# rubocop:enable Layout/LineLength
