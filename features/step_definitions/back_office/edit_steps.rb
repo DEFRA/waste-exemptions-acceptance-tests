6ea8af2d76ee0c73fab17fad647fc9d81bc3bc92
Tim Stone <tim.stone@yahoo.co.uk>
2025-04-01 10:30:15 +0100

    Registration edit change history step updates (#418)

--- features/step_definitions/back_office/edit_steps.rb
+++ features/step_definitions/back_office/edit_steps.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-When("I edit the most recent registration") do
+When("I edit the applicants phone number") do
   # Last registration number is stored in @world.last_reg_no.
   # Search for the last reference number:
   @world.bo.dashboard_page.submit(search_term: @world.last_reg_no)
@@ -11,28 +11,50 @@ When("I edit the most recent registration") do
   @new_details = generate_registration(:individual, "Mr Waste")
   @new_person = generate_person("wex@example.com")
 
-  # Edit the applicant's name to something random
-  @world.bo.edit_page.change_applicant_link.click
-  @world.journey.name_page.submit(
-    first_name: @new_person[:first_name],
-    last_name: @new_person[:last_name]
-  )
+  @world.bo.edit_page.change_applicant_phone.click
+  @updated_detail = @new_person[:telephone]
+  @world.journey.phone_page.submit(tel_no: @updated_detail)
+  @change_reason = "Applicant phone number changed"
+end
+
+When("I edit the contact email address") do
+  # Last registration number is stored in @world.last_reg_no.
+  # Search for the last reference number:
+  @world.bo.dashboard_page.submit(search_term: @world.last_reg_no)
+  find_link("Edit").click
+  expect(@world.bo.edit_page.heading).to have_text("Edit #{@world.last_reg_no} registration")
+
+  # Generate data using the functions in data_generator.rb
+  @new_details = generate_registration(:individual, "Mr Waste")
+  @new_person = generate_person("wex@example.com")
 
   # Edit the contact email
   @world.bo.edit_page.change_contact_email_link.click
-
+  @updated_detail = @new_person[:email]
   @world.journey.email_page.submit(
     email: @new_person[:email],
     confirm_email: @new_person[:email]
   )
+  @change_reason = "Contact email address changed"
+end
 
+When("I edit the most recent registration") do
+  @world.bo.dashboard_page.submit(search_term: @world.last_reg_no)
+  find_link("Edit").click
+  # Generate data using the functions in data_generator.rb
+  @new_details = generate_registration(:individual, "Mr Waste")
+  @new_person = generate_person("wex@example.com")
+
+  # Edit the contact email
+  @world.bo.edit_page.change_contact_email_link.click
+  @world.journey.email_page.submit(
+    email: @new_person[:email],
+    confirm_email: @new_person[:email]
+  )
 end
 
 Then("I can see the new details on the registration details page") do
-  expect(@world.bo.registration_details_page).to have_text(@new_person[:first_name])
-  expect(@world.bo.registration_details_page).to have_text(@new_person[:last_name])
-  expect(@world.bo.registration_details_page).to have_text(@new_person[:email])
-  expect(@world.bo.registration_details_page).to have_text(@new_person[:first_name])
+  expect(@world.bo.registration_details_page).to have_text(@updated_detail)
 end
 
 When("I cannot edit the most recent registration") do
@@ -49,7 +71,7 @@ end
 
 When("I complete the edit") do
   @world.bo.edit_page.submit
-  @world.bo.edit_reason_page.submit(reason: "Requested by customer")
+  @world.bo.edit_reason_page.submit(reason: @change_reason)
   @world.journey.declaration_page.submit
   expect(@world.bo.edit_details_page.heading).to have_text("Edit complete")
   puts "#{@world.last_reg_no} edited"
@@ -66,6 +88,5 @@ When("I cancel the edit") do
 end
 
 When("I cannot see the new details on the registration details page") do
-  expect(@world.bo.registration_details_page).not_to have_text(@new_person[:first_name])
-  expect(@world.bo.registration_details_page).not_to have_text(@new_person[:last_name])
+  expect(@world.bo.registration_details_page).not_to have_text(@new_person[:email])
 end
