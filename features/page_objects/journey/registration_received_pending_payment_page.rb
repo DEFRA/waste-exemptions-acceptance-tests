480e9c3ae2f0e221517368adbd8202b629deec67
timstone <tim.stone@yahoo.co.uk>
2025-04-15 14:11:57 +0100

    Payment pending confirmation selectors update

--- features/page_objects/journey/registration_received_pending_payment_page.rb
+++ features/page_objects/journey/registration_received_pending_payment_page.rb
@@ -2,7 +2,7 @@
 
 class RegistrationReceivedPendingPaymentPage < BasePage
 
-  element(:registration_number, ".govuk-summary-list__row:nth-child(3) strong")
-  element(:payment_amount, ".govuk-summary-list__row:nth-child(4) strong")
+  element(:registration_number, ".govuk-summary-list__row:nth-child(4) strong")
+  element(:payment_amount, ".govuk-summary-list__row:nth-child(5) strong")
 
 end
