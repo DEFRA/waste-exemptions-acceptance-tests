3d7f1c124154aeae6783a1972d2e6fbc5afb90cb
timstone <tim.stone@yahoo.co.uk>
2025-05-23 10:08:48 +0100

    Linting fix

--- features/step_definitions/back_office/exemption_expiry_date_extension_steps.rb
+++ features/step_definitions/back_office/exemption_expiry_date_extension_steps.rb
@@ -12,7 +12,8 @@ When("I extend the registration's exemption expiry date") do
                                            year: @new_date.strftime("%Y"),
                                            reason: @change_reason)
 end
-
+# rubocop:disable Layout/LineLength
 Then("I can see the exemption expiry date has extended") do
   expect(@world.bo.registration_details_page.exemption_details.first.expiry_date.text).to eq(@new_date.strftime("%d %B %Y"))
 end
+# rubocop:enable Layout/LineLength
