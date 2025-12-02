89e0e84cf9ea0359c225d7eb55d08cc67d04aa48
timstone <tim.stone@yahoo.co.uk>
2025-12-02 15:12:27 +0000

    Linear test fix

--- features/step_definitions/journey/linear_registration_steps.rb
+++ features/step_definitions/journey/linear_registration_steps.rb
@@ -14,8 +14,9 @@ end
 
 Then("I can view linear registration's details from the linear registrations list") do
   @world.bo.dashboard_page.admin_menu.linear_registrations.click
-  @world.bo.linear_registrations_page.wait_until_first_view_registration_link_visible
   @world.bo.linear_registrations_page.last_page.click if @world.bo.linear_registrations_page.has_last_page?
+  @world.bo.linear_registrations_page.wait_until_first_view_registration_link_visible
+  
   linear_registration = @world.bo.linear_registrations_page.linear_regs(@world.last_reg_no)
   linear_registration.view_registration.click
   puts current_url
